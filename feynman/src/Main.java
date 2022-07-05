8a6b812e7db9512ba50927db597c59578f7a4a79
Maciej Puzianowski <maciej.puzianowski@opi.org.pl>
2022-07-05 15:22:17 +0200

    feynman

--- feynman/src/Main.java
+++ feynman/src/Main.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s = r.readLine()) != null){
+            int result =feynman(Integer.parseInt(s));
+            System.out.println(result == 0 ? "" : result);
+        }
     }
+
+    public static int feynman(int i){
+        if(i == 0) return 0;
+        if(i == 1) return 1;
+        return feynman(i-1) + (i*i);
+    }
+
 }
