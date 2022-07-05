9301a7765b091ef563903203dc9cb6d77ad18404
Maciej Puzianowski <maciej.puzianowski@opi.org.pl>
2022-07-05 12:33:08 +0200

    initial

--- prime1/src/Main.java
+++ prime1/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -6,14 +7,52 @@ public class Main {
         String s;
         while((s = r.readLine()) != null){
             primes(s);
-
         }
     }
 
+    //https://pl.wikipedia.org/wiki/Sito_Eratostenesa#/media/Plik:Sieve_of_Eratosthenes_animation.gif
+
     public static void primes(String input){
+        var splitedInput = input.split(" ");
 
+        if(splitedInput.length == 2){
+            int start = Integer.parseInt(splitedInput[0]), stop = Integer.parseInt(splitedInput[1]);
+            //looking for primes up to sqrt of stop
+            int j = 0;
+            int sqrt = (int) Math.sqrt(stop);
+            boolean[] isPrime = new boolean[sqrt+1];
+            int[] primes = new int[sqrt+1];
+            Arrays.fill(isPrime, true);
+            for (int i = 2; i <= sqrt; i++){
+                if(isPrime[i]){
+                    primes[j] = i;
+                    j++;
+                    for(int k = i+i; k<= sqrt; k+=i){
+                        isPrime[k] = false;
+                    }
+                }
+            }
+
+            //looking for primes in range
+            int size = stop - start + 1;
+            isPrime = new boolean[size];
+            Arrays.fill(isPrime, true);
+            for(int k = 0; k < j; k++){
+                int closestInt = start / primes[k];
+                closestInt *= primes[k];
+                while(closestInt <= stop){
+                    if(closestInt >= start && primes[k] != closestInt) isPrime[closestInt-start] = false;
+                    closestInt += primes[k];
+                }
+            }
+            for(int i = 0; i < size; i++){
+                if(isPrime[i] && (i+start) != 1){
+                    System.out.println(i+start);
+                }
+            }
+            System.out.println();
+        }
     }
-
 
     //
     //
