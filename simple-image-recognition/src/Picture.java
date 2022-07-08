a4332fc58e487b51f242a204a2943e15c2abcb33
Maciej Puzianowski <maciej.puzianowski@opi.org.pl>
2022-07-08 11:14:22 +0200

    56 score s-i-r

--- simple-image-recognition/src/Picture.java
+++ simple-image-recognition/src/Picture.java
@@ -5,16 +5,21 @@ class Picture {
     private final int Width;
     private final int Height;
     private final String[] Data;
+    private final char[][] Chars;
     private final ArrayList<Point> Points;
 
     Picture(int w, int h, String[] d){
         Width = w;
         Height = h;
         Data = d;
+        Chars = new char[Height][Width];
         Points = new ArrayList<>();
-        for(int i = 0; i < Data.length; i++){
+        for(int i = 0; i < Data.length; i++) {
             for (int k = 0; k < Width; k++) {
-                if(Data[i].charAt(k) == 'x') Points.add(new Point(k+1, i+1));
+                if (Data[i].charAt(k) == 'x') {
+                    Chars[i][k] = 'x';
+                    Points.add(new Point(k + 1, i + 1));
+                } else Chars[i][k] = '.';
             }
         }
     }
@@ -66,5 +71,43 @@ class Picture {
         }
         if(parallelcounter > 1) outcome = "0";
         return outcome;
+    }
+
+    public String lessScoreWhatAmI(){
+        char[][] pat1 = new char[3][3];
+        pat1[0][0] = 'x';
+        pat1[0][1] = 'x';
+        pat1[0][2] = 'x';
+        pat1[1][0] = 'x';
+        pat1[1][1] = '.';
+        pat1[1][2] = '.';
+        pat1[2][0] = 'x';
+        pat1[2][1] = '.';
+        pat1[2][2] = '.';
+        char[][] pat2 = new char[3][3];
+        pat2[0][0] = '.';
+        pat2[0][1] = 'x';
+        pat2[0][2] = 'x';
+        pat2[1][0] = 'x';
+        pat2[1][1] = '.';
+        pat2[1][2] = '.';
+        pat2[2][0] = 'x';
+        pat2[2][1] = '.';
+        pat2[2][2] = '.';
+        for (int k = 0; k < Height - 3; k++) {
+            for (int i = 0; i < Width - 3; i++) {
+                if ((Chars[k][i] == pat1[0][0] && Chars[k][i + 1] == pat1[0][1] && Chars[k][i + 2] == pat1[0][2]
+                        && Chars[k + 1][i] == pat1[1][0] && Chars[k + 1][i + 1] == pat1[1][1]
+                        && Chars[k + 2][i] == pat1[2][0]) ||
+                        (Chars[k][i] == pat2[0][0] && Chars[k][i + 1] == pat2[0][1] && Chars[k][i + 2] == pat2[0][2]
+                                && Chars[k + 1][i] == pat2[1][0] && Chars[k + 1][i + 1] == pat2[1][1] && Chars[k + 2][i] == pat2[2][0])) {
+                    if(i > 0 && k > 0){
+                        if(Chars[k-1][i] == 'x' || Chars[k][i-1] == 'x') return "x";
+                    }
+                    return "0";
+                }
+            }
+        }
+        return "x";
     }
 }
