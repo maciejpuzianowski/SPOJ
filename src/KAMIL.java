b3aa30aa2afa7598acc1bd2306bed7c14b119ab2
Maciej Puzianowski <maciej.puzianowski@opi.org.pl>
2022-07-12 14:47:56 +0200

    KAMIL 234 optimized

--- src/KAMIL.java
+++ src/KAMIL.java
@@ -1,1 +1,1 @@
-import java.util.*;class KAMIL{public static void main(String[] args){Scanner s=new Scanner(System.in);int i=0,f=0;for(;i<10;i++){int r=1;for(char c:s.nextLine().toCharArray()){var a=(c=='T'||c=='D'||c=='L'||c=='F')?r*=2:f;}System.out.println(r);}}}
+class KAMIL{public static void main(String[] args){var s=new java.util.Scanner(System.in);int i=0,f=0;for(;i<10;i++){int r=1;for(var c:s.nextLine().toCharArray()){var a=(c=='T'||c=='D'||c=='L'||c=='F')?r*=2:f;}System.out.println(r);}}}
